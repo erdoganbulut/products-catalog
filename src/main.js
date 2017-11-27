daec5c9507be49d644ad4f8d2b0806ab315dd473
Erdoğan Bulut <erdgnblt@gmail.com>
2017-11-27 04:46:09 +0300

    Vue resource install and import

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+// Vue resource
+import VueResource from 'vue-resource';
+
 import App from './App';
 import router from './router';
 
 Vue.use(BootstrapVue);
+
+Vue.use(VueResource);
 
 Vue.config.productionTip = false;
 
