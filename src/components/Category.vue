f719f19154d9c3198339011477b9f98cebab7fe4
Erdoğan Bulut <erdgnblt@gmail.com>
2017-11-27 12:07:57 +0300

    Create category component and import category banner

---
+++ src/components/Category.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="page">
+    <header-bar />
+    <category-banner />
+  </main>
+</template>
+
+<script>
+import HeaderBar from './shareds/HeaderBar';
+import CategoryBanner from './shareds/CategoryBanner';
+
+export default {
+  name: 'Category',
+  data() {
+    return {
+    };
+  },
+  components: {
+    HeaderBar,
+    CategoryBanner,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import '../scss/shareds';
+</style>
