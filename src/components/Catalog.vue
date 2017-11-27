37aab145c4936f316628a2835e0a964bf1c28b0d
Erdoğan Bulut <erdgnblt@gmail.com>
2017-11-27 07:23:37 +0300

    Create catalog page component

---
+++ src/components/Catalog.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="page">
+    <header-bar />
+    <catalog-list />
+  </main>
+</template>
+
+<script>
+import HeaderBar from './shareds/HeaderBar';
+import CatalogList from './shareds/CatalogList';
+
+export default {
+  name: 'Catalog',
+  data() {
+    return {
+    };
+  },
+  components: {
+    HeaderBar,
+    CatalogList,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import '../scss/shareds';
+</style>
