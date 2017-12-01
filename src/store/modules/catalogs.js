0cf9000048c63ebf1055ebdcdd529885ba4d5ac2
Erdogan Bulut <e-bulut@hotmail.com.tr>
2017-12-01 18:01:57 +0300

    Create catalog store

---
+++ src/store/modules/catalogs.js
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+
+const getters = {
+  catalogs: state => state.catalogs,
+  status: state => state.status,
+  response: state => state.response,
+};
+
+const actions = {
+  getCatalog({ commit }) {
+    Vue.http.get('./static/api/catalogs.json').then((response) => {
+      const Response = response;
+      commit('receiveCatalogs', Response.body);
+      commit('receiveStatus', 'done');
+      commit('receiveResponse', Response);
+    }, (response) => {
+      const Response = response;
+      commit('receiveCatalogs', {});
+      commit('receiveStatus', 'error');
+      commit('receiveResponse', Response);
+    });
+  },
+};
+
+const state = {
+  catalogs: {},
+  status: 'non-request',
+  response: {},
+};
+
+const mutations = {
+  receiveCatalogs(State, catalogs) {
+    state.catalogs = catalogs;
+  },
+  receiveStatus(State, status) {
+    state.status = status;
+  },
+  receiveResponse(State, response) {
+    state.response = response;
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
