619a28728452703c45882b565a174f11dfaaeca0
Erdogan Bulut <e-bulut@hotmail.com.tr>
2017-12-01 18:02:41 +0300

    Using store to CatalogList

--- src/components/shareds/CatalogList.vue
+++ src/components/shareds/CatalogList.vue
@@ -2,24 +2,9 @@
   <section class="catalog-list-component">
     <div class="container">
       <div class="catalog-items">
-        <div class="catalog-item">
-          <router-link to="/" class="catalog-item-inner">
-            <img src="http://placehold.it/1080x1080" alt="">
-          </router-link>
-        </div>
-        <div class="catalog-item">
-          <router-link to="/" class="catalog-item-inner">
-            <img src="http://placehold.it/1080x1080" alt="">
-          </router-link>
-        </div>
-        <div class="catalog-item">
-          <router-link to="/" class="catalog-item-inner">
-            <img src="http://placehold.it/1080x1080" alt="">
-          </router-link>
-        </div>
-        <div class="catalog-item">
-          <router-link to="/" class="catalog-item-inner">
-            <img src="http://placehold.it/1080x1080" alt="">
+        <div class="catalog-item" v-for="catalog in catalogs.catalogItems">
+          <router-link :to="'/' + lang.url + '/catalog/' + catalog.url" class="catalog-item-inner">
+            <img :src="catalog.photo" :alt="catalog.name">
           </router-link>
         </div>
       </div>
@@ -28,11 +13,29 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'CatalogList',
   data() {
     return {
     };
+  },
+  computed: {
+    ...mapGetters({
+      catalogs: 'catalogs/catalogs',
+      status: 'catalogs/status',
+      response: 'catalogs/response',
+      lang: 'lang/lang',
+    }),
+  },
+  methods: {
+    getCatalog() {
+      this.$store.dispatch('catalogs/getCatalog');
+    },
+  },
+  mounted() {
+    this.getCatalog();
   },
 };
 </script>
