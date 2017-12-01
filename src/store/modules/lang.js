392d9802b51e5bb926f187cf4b44219f4468257d
Erdogan Bulut <e-bulut@hotmail.com.tr>
2017-12-01 18:01:47 +0300

    Fix lang store for using namespace, add LangList, add LangListStatus and beautify code

--- src/store/modules/lang.js
+++ src/store/modules/lang.js
@@ -1,32 +1,66 @@
 import Vue from 'vue';
 
 const getters = {
-  allLang: state => state.all,
+  lang: state => state.lang,
+  langList: state => state.langList,
+  langListStatus: state => state.langListStatus,
 };
 
 const actions = {
   setLang({ commit }, lang) {
     Vue.http.get(`./static/api/lang_${lang}.json`).then((response) => {
-      const getData = response.body;
-      commit('receiveLang', getData);
+      const Response = response;
+      commit('receiveLang', Response.body);
+      commit('receiveLangResponse', Response);
     }, (response) => {
-      console.log(response);
+      const Response = response;
+      commit('receiveLang', {});
+      commit('receiveLangResponse', Response);
+    });
+  },
+  getLangList({ commit }) {
+    Vue.http.get('./static/api/lang_list.json').then((response) => {
+      const Response = response;
+      commit('receiveLangList', Response.body);
+      commit('receiveLangListStatus', 'done');
+      commit('receiveLangListResponse', Response);
+    }, (response) => {
+      const Response = response;
+      commit('receiveLangList', {});
+      commit('receiveLangListStatus', 'error');
+      commit('receiveLangListResponse', Response);
     });
   },
 };
 
 const state = {
-  all: {},
+  lang: {},
+  langResponse: {},
+  langList: {},
+  langListStatus: 'non-request',
+  langListResponse: {},
 };
 
 const mutations = {
   receiveLang(State, lang) {
-    state.all = lang;
-    console.log(state.all);
+    state.lang = lang;
+  },
+  receiveLangResponse(State, response) {
+    state.langResponse = response;
+  },
+  receiveLangList(State, langList) {
+    state.langList = langList;
+  },
+  receiveLangListStatus(State, status) {
+    state.langListStatus = status;
+  },
+  receiveLangListResponse(State, response) {
+    state.langListResponse = response;
   },
 };
 
 export default {
+  namespaced: true,
   state,
   getters,
   actions,
