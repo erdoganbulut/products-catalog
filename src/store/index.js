c87fcdc7bf26ef801d65a4278cc7cb0ec2e62548
Erdogan Bulut <ebulut@protel.com.tr>
2017-12-14 19:20:34 +0300

    import category store module to main store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import moduleLang from './modules/lang';
 import moduleCatalogs from './modules/catalogs';
 import moduleCategories from './modules/categories';
+import moduleCategory from './modules/category';
 
 export default {
   modules: {
     lang: moduleLang,
     catalogs: moduleCatalogs,
     categories: moduleCategories,
+    category: moduleCategory,
   },
 };
