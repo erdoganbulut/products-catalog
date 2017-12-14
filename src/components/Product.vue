2bf270d43aab27d3038e5f1a2acee08f120e3679
Erdogan Bulut <ebulut@protel.com.tr>
2017-12-14 19:34:44 +0300

    create product component

---
+++ src/components/Product.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="page">
+    <header-bar />
+  </main>
+</template>
+
+<script>
+import HeaderBar from './shareds/HeaderBar';
+
+export default {
+  name: 'Product',
+  data() {
+    return {
+    };
+  },
+  components: {
+    HeaderBar,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import '../scss/shareds';
+</style>
